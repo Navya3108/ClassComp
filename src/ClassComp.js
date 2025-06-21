b87f24519cae3aeced6deb949188ded2aea7c04f
Navya <anavya31@gmail.com>
2025-06-21 15:33:04 +0530

    classcomp

--- src/ClassComp.js
+++ src/ClassComp.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class ClassComp extends Component {
-  // 1. Initialize state in the constructor
   constructor(props) {
     super(props);
     this.state = {
@@ -9,33 +8,25 @@ class ClassComp extends Component {
     };
   }
 
-  // 2. Create methods to handle state changes
-  // Use arrow functions to automatically bind 'this'
-  inchandler = () => {
-    // Use this.setState to update the state
-    this.setState(prevState => ({
+  increment = () => {
+    this.setState((prevState) => ({
       count: prevState.count + 1,
     }));
   };
 
-  dechandler = () => {
-    this.setState(prevState => ({
+  decrement = () => {
+    this.setState((prevState) => ({
       count: prevState.count - 1,
     }));
   };
 
-  // 3. The render method returns the component's UI
   render() {
     return (
-      <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+      <div>
         <h1>Counter App</h1>
-        {/* Access state using this.state.count */}
-        <h3>Count: {this.state.count}</h3>
-        <div style={{ display: "flex", gap: "10px", marginTop: "10px" }}>
-          {/* Call the methods from the class instance */}
-          <button onClick={this.inchandler}>Increment</button>
-          <button onClick={this.dechandler}>Decrement</button>
-        </div>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
       </div>
     );
   }
